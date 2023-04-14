5c1a513943bb327dbc004d4d9021139a96977854
chumpa <chumpa@yandex.ru>
2023-04-14 20:30:20 +0300

    показ XI-протокола

--- src/main/kotlin/karlutka/parsers/pi/XiMessage.kt
+++ src/main/kotlin/karlutka/parsers/pi/XiMessage.kt
@@ -1,120 +1,149 @@
 package karlutka.parsers.pi
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.dom.Element
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 import nl.adaptivity.xmlutil.serialization.XmlValue
+import nl.adaptivity.xmlutil.util.CompactFragment
+
+const val xmlnsSOAP: String = "http://schemas.xmlsoap.org/soap/envelope/"
+const val xmlnsXI30: String = "http://sap.com/xi/XI/Message/30"
+const val xmlnsXI30prefix: String = "SAP"
 
 class XiMessage {
+    enum class PayloadType {Application, ApplicationAttachment}
+    enum class QOS {ExactlyOnce, ExactlyOnceInOrder}
+
     @Serializable
-    @XmlSerialName("Envelope", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
-    class XSOAP(
+    @XmlSerialName("Envelope", xmlnsSOAP, "SOAP")
+    class Envelope(
         @XmlElement(true)
-        val Header: XHeader,
+        val Header: Header,
         @XmlElement(true)
-        val Body: XBody
+        val Body: Body,
     )
 
     @Serializable
-    @XmlSerialName("Header", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
-    class XHeader(
+    @XmlSerialName("Header", xmlnsSOAP, "SOAP")
+    class Header(
         @XmlElement(true)
-        val Main: XMain,
+        val Main: Main,
         @XmlElement(true)
-        val ReliableMessaging: XReliableMessaging,
+        val ReliableMessaging: ReliableMessaging,
         @XmlElement(true)
-        val System: XSystem,
+        val DynamicConfiguration: DynamicConfiguration? = null,
         @XmlElement(true)
-        val Diagnostic: XDiagnostic,
+        val System: System? = null,
         @XmlElement(true)
-        val HopList: XHopList,
+        val Diagnostic: Diagnostic? = null,
         @XmlElement(true)
-        val Passport: XPassport? = null,
+        val HopList: HopList? = null,
         @XmlElement(true)
-        val DynamicConfiguration: XDynamicConfiguration,
+        val Passport: Passport? = null,
+        @XmlElement(true)
+        @XmlSerialName("RunTime", xmlnsXI30, xmlnsXI30prefix)
+        @Contextual
+        val RunTime: CompactFragment? = null,
+        @XmlElement(true)
+        @XmlSerialName("PerformanceHeader", xmlnsXI30, xmlnsXI30prefix)
+        @Contextual
+        val PerformanceHeader: CompactFragment? = null,
+        @XmlElement(true)
+        @XmlSerialName("Trace", xmlnsXI30, xmlnsXI30prefix)
+        @Contextual
+        val Trace: CompactFragment? = null,
     )
 
     @Serializable
-    @XmlSerialName("Body", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
-    class XBody(
+    @XmlSerialName("Body", xmlnsSOAP, "SOAP")
+    class Body(
         @XmlElement(true)
-        val Manifest: XManifest,
+        val Manifest: Manifest,
     )
 
     @Serializable
-    @XmlSerialName("Main", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XMain(
+    @XmlSerialName("Main", xmlnsXI30, xmlnsXI30prefix)
+    class Main(
         val versionMajor: Int = 3,
         val versionMinor: Int = 1,
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
         val soapMustUnderstand: Int = 1,
         @XmlSerialName("Id", "http://www.docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "wsu")
         val wsuId: String? = null,
         @XmlElement(true)
-        val MessageClass: String = "ApplicationMessage",
+        val MessageClass: String, // = "ApplicationMessage",
         @XmlElement(true)
-        val ProcessingMode: String = "asynchronous",
+        val ProcessingMode: String, // = "asynchronous",
         @XmlElement(true)
-        val MessageId: String = "005056bf-38e0-1edd-b58c-9b5814a5e450",
+        val MessageId: String,
         @XmlElement(true)
-        val TimeSent: String = "2023-04-06T10:22:01Z",
+        val RefToMessageId: String? = null,
         @XmlElement(true)
-        @XmlSerialName("Sender", "http://sap.com/xi/XI/Message/30", "SAP")
-        val Sender: XPartyService,
+        val TimeSent: String,
         @XmlElement(true)
-        @XmlSerialName("Receiver", "http://sap.com/xi/XI/Message/30", "SAP")
-        val Receiver: XPartyService,
+        @XmlSerialName("Sender", xmlnsXI30, xmlnsXI30prefix)
+        val Sender: PartyService,
         @XmlElement(true)
-        val Interface: XInterface,
+        @XmlSerialName("Receiver", xmlnsXI30, xmlnsXI30prefix)
+        val Receiver: PartyService? = null,
+        @XmlElement(true)
+        val Interface: Interface? = null,
     )
 
     @Serializable
-    class XPartyService(
+    class PartyService(
         @XmlElement(true)
-        val Party: XParty,
+        val Party: Party? = null,
         @XmlElement(true)
-        val Service: String
+        val Service: String? = null,
+        @XmlElement(true)
+        val Interface: Interface? = null
     )
 
     @Serializable
-    @XmlSerialName("Party", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XParty(
+    @XmlSerialName("Party", xmlnsXI30, xmlnsXI30prefix)
+    class Party(
+        @XmlValue(true)
+        val content: String? = null,
         val agency: String = "http://sap.com/xi/XI",
         val scheme: String = "XIParty",
-        @XmlValue(true)
-        val content: String = ""
     )
 
     @Serializable
-    @XmlSerialName("Interface", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XInterface(
+    @XmlSerialName("Interface", xmlnsXI30, xmlnsXI30prefix)
+    class Interface(
         val namespace: String,
         @XmlValue(true)
         val content: String = ""
     )
 
     @Serializable
-    @XmlSerialName("ReliableMessaging", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XReliableMessaging(
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
-        val soapMustUnderstand: Int = 1,
+    @XmlSerialName("ReliableMessaging", xmlnsXI30, xmlnsXI30prefix)
+    class ReliableMessaging(
         @XmlElement(true)
-        val QualityOfService: String = "ExactlyOnce"
+        @XmlSerialName("QualityOfService", xmlnsXI30, xmlnsXI30prefix)
+        val QualityOfService: QOS,
+        @XmlElement(true)
+        val QueueId: String? = null,
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
+        val soapMustUnderstand: Int = 1
     )
 
     @Serializable
-    @XmlSerialName("System", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XSystem(
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
+    @XmlSerialName("System", xmlnsXI30, xmlnsXI30prefix)
+    class System(
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
         val soapMustUnderstand: Int = 1,
         @XmlElement(true)
-        @XmlSerialName("Record", "http://sap.com/xi/XI/Message/30", "SAP")
-        val Record: List<XRecord>
+        @XmlSerialName("Record", xmlnsXI30, xmlnsXI30prefix)
+        val Record: List<Record>
     )
 
     @Serializable
-    @XmlSerialName("Record", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XRecord(
+    @XmlSerialName("Record", xmlnsXI30, xmlnsXI30prefix)
+    class Record(
         val namespace: String,
         val name: String,
         @XmlValue(true)
@@ -122,9 +151,9 @@ class XiMessage {
     )
 
     @Serializable
-    @XmlSerialName("Diagnostic", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XDiagnostic(
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
+    @XmlSerialName("Diagnostic", xmlnsXI30, xmlnsXI30prefix)
+    class Diagnostic(
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
         val soapMustUnderstand: Int = 1,
         @XmlElement(true)
         val TraceLevel: String,
@@ -133,23 +162,23 @@ class XiMessage {
     )
 
     @Serializable
-    @XmlSerialName("HopList", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XHopList(
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
+    @XmlSerialName("HopList", xmlnsXI30, xmlnsXI30prefix)
+    class HopList(
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
         val soapMustUnderstand: Int = 1,
         @XmlElement(true)
-        @XmlSerialName("Hop", "http://sap.com/xi/XI/Message/30", "SAP")
-        val HopList: List<XHop>
+        @XmlSerialName("Hop", xmlnsXI30, xmlnsXI30prefix)
+        val HopList: List<Hop>
     )
 
     @Serializable
-    class XHop(
+    class Hop(
         val timeStamp: String,
         val wasRead: Boolean,
         @XmlElement(true)
-        val Engine: XEngine,
+        val Engine: Engine,
         @XmlElement(true)
-        val Adapter: XAdapter,
+        val Adapter: Adapter,
         @XmlElement(true)
         val MessageId: String,
         @XmlElement(true)
@@ -157,24 +186,24 @@ class XiMessage {
     )
 
     @Serializable
-    @XmlSerialName("Engine", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XEngine(
-        val type: String = "BS",
+    @XmlSerialName("Engine", xmlnsXI30, xmlnsXI30prefix)
+    class Engine(
+        val type: String,           //BS, IS
         @XmlValue(true)
-        val content: String
+        val content: String?
     )
 
     @Serializable
-    @XmlSerialName("Adapter", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XAdapter(
+    @XmlSerialName("Adapter", xmlnsXI30, xmlnsXI30prefix)
+    class Adapter(
         val namespace: String = "http://sap.com/xi/XI/System",
         @XmlValue(true)
         val content: String = "XI"
     )
 
     @Serializable
-    @XmlSerialName("Passport", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XPassport(
+    @XmlSerialName("Passport", xmlnsXI30, xmlnsXI30prefix)
+    class Passport(
         @XmlElement(true)
         val PassportHash: String,
         @XmlElement(true)
@@ -182,35 +211,43 @@ class XiMessage {
     )
 
     @Serializable
-    @XmlSerialName("DynamicConfiguration", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XDynamicConfiguration(
-        @XmlSerialName("mustUnderstand", "http://schemas.xmlsoap.org/soap/envelope/", "SOAP")
-        val soapMustUnderstand: Int = 1,
+    @XmlSerialName("DynamicConfiguration", xmlnsXI30, xmlnsXI30prefix)
+    class DynamicConfiguration(
         @XmlElement(true)
-        val Record: List<XRecord>
+        val Record: List<Record>,
+        @XmlSerialName("mustUnderstand", xmlnsSOAP, "SOAP")
+        val soapMustUnderstand: Int = 1,
     )
 
     @Serializable
-    @XmlSerialName("Manifest", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XManifest(
+    @XmlSerialName("Manifest", xmlnsXI30, xmlnsXI30prefix)
+    class Manifest(
         @XmlSerialName("Id", "http://www.docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "wsu")
         val wsuId: String? = null,
         @XmlElement(true)
-        val Payload: XPayload
+        @XmlSerialName("Payload", xmlnsXI30, xmlnsXI30prefix)
+        val Payload: List<Payload>
     )
 
     @Serializable
-    @XmlSerialName("Payload", "http://sap.com/xi/XI/Message/30", "SAP")
-    class XPayload(
+    class Payload(
         @XmlSerialName("type", "http://www.w3.org/1999/xlink", "xlink")
-        val xlinktype: String, // simple
+        val xlinktype: String? = null, // simple. Пусто в абапе
         @XmlSerialName("href", "http://www.w3.org/1999/xlink", "xlink")
         val href: String, // cid:payload-005056BF38E01EDDB58C9B5814A60450@sap.com
         @XmlElement(true)
-        val Name: String,
+        val Name: String,           //
         @XmlElement(true)
-        val Description: String,
+        @XmlSerialName("Type", xmlnsXI30, xmlnsXI30prefix)
+        val Type: PayloadType,
         @XmlElement(true)
-        val Type: String,
+        val Description: String? = null,
+    )
+
+    @Serializable
+    @XmlSerialName("RunTime", xmlnsXI30, xmlnsXI30prefix)
+    class RunTime(                      // Только в абапе
+        @XmlValue(true)
+        val content: List<Element>
     )
 }
--- src/test/kotlin/KXiMessageTest.kt
+++ src/test/kotlin/KXiMessageTest.kt
@@ -1,29 +1,271 @@
+import KT.Companion.ris
 import KT.Companion.x
 import karlutka.parsers.pi.XiMessage
 import karlutka.serialization.KSoap
 import kotlinx.serialization.encodeToString
+import nl.adaptivity.xmlutil.serialization.XML
+import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+import javax.activation.DataHandler
+import javax.mail.Multipart
+import javax.mail.Session
+import javax.mail.internet.*
+import javax.mail.util.ByteArrayDataSource
+import javax.net.ssl.HttpsURLConnection
 import kotlin.test.Test
 
+
 class KXiMessageTest {
+    val xmlserializer = XML(KSoap.xmlmodule) {
+        autoPolymorphic = true
+    }
+
     @Test
     fun constructor() {
-        val sender = XiMessage.XPartyService(XiMessage.XParty("agency", "schema", ""), "DER200")
-        val receiver = XiMessage.XPartyService(XiMessage.XParty("agency", "schema", ""), "BS_DigitalPlatformKafka_D")
-        val iface = XiMessage.XInterface("urn:vendor:po:I:DIGITALPLATFORMKAFKA:India:CharTransfering", "SI_CharTransfering_InAsync")
-        val main = XiMessage.XMain(
-            3, 1, 1, null, "ApplicationMessage", "asynchronous", "005056bf-38e0-1edd-b58c-9b5814a5e450", "2023-04-06T10:22:01Z",
+        val messageid = "00000000-0000-0000-0000-000000000025"
+        val datetime = "2023-04-14T11:31:01Z"
+        val sender = XiMessage.PartyService(XiMessage.Party(), "BC_TEST1")
+        val receiver = XiMessage.PartyService(XiMessage.Party(), "")
+        val iface = XiMessage.Interface("urn:none", "SI_OutAsync")
+        val payload = "<РусскиеБуквыПобеждаютЪ> С ПЯТНИЦЕЙ!  </РусскиеБуквыПобеждаютЪ>"
+        val dyn = XiMessage.DynamicConfiguration(listOf(
+            XiMessage.Record("urn:nlmk", "NLMK1", "РусскиеБуквы в DC"),
+            XiMessage.Record("urn:sapintegration", "FRIDAY_20230414", "РусскиеБуквы в DC"),
+            )
+        )
+        val _msid = messageid.replace("-", "")
+        val cidSoap = "soap-$_msid@sap.com"
+        val cidPayl = "payload-$_msid@sap.com"
+        val cidAttach1 = "attach1-$_msid@sap.com"
+        val cidAttach2 = "attach2-$_msid@sap.com"
+        val cidAttach3 = "attach3-$_msid@sap.com"
+
+        val main = XiMessage.Main(
+            3, 1, 1, null, "ApplicationMessage", "asynchronous", messageid, null, datetime,
             sender, receiver, iface
         )
-        val smain = KSoap.xmlserializer.encodeToString(main)
 
-        val srecord = KSoap.xmlserializer.encodeToString(XiMessage.XRecord("http://sap.com/xi/XI/Message/30/general", "interfaceDeterminationGUID", "830791ead46411edb906000000417ca6"))
-        println(srecord)
+        val eo = XiMessage.ReliableMessaging(XiMessage.QOS.ExactlyOnce)
+        //val eoio = XiMessage.ReliableMessaging(XiMessage.QOS.ExactlyOnceInOrder, "_FRIDAY")
 
+        val payload1 = XiMessage.Payload("simple", "cid:$cidPayl", "Main20230414", XiMessage.PayloadType.Application)
+        val attachment1 = XiMessage.Payload("simple", "cid:$cidAttach1", "Attachment1", XiMessage.PayloadType.ApplicationAttachment)
+        val attachment2 = XiMessage.Payload("simple", "cid:$cidAttach2", "Attachment2", XiMessage.PayloadType.ApplicationAttachment)
+        val attachment3 = XiMessage.Payload("simple", "cid:$cidAttach3", "Attachment3", XiMessage.PayloadType.ApplicationAttachment)
+        val manifest = XiMessage.Manifest(null, listOf(payload1, attachment1, attachment2, attachment3))
+        val envelope = XiMessage.Envelope(XiMessage.Header(main, eo, dyn), XiMessage.Body(manifest))
+        val smain = xmlserializer.encodeToString(envelope) + "\n"
+
+        val mp: MimeMultipart = MimeMultipart("related")
+        val ct = ContentType(mp.contentType)
+        val boundary = ct.getParameter("boundary")
+
+        val pSoap = MimeBodyPart(headers(cidSoap, "text/xml; charset=utf-8", "attachment; filename=\"$cidSoap.xml\""), smain.encodeToByteArray())
+        val pPayl = MimeBodyPart(headers(cidPayl, "application/xml; charset=utf-8", "attachment; filename=\"Payload.xml\""), payload.toByteArray())
+        val pAtt1 = MimeBodyPart(headers(cidAttach1, "text/plain; charset=utf-8", "attachment; filename=\"Att1.xml\""), "Русскiя  1111111111 аттача1".toByteArray())
+        val pAtt2 = MimeBodyPart(headers(cidAttach2, "text/plain; charset=utf-8", "attachment; filename=\"Att2.xml\""), "Русскiя 2 22222 аттача2".toByteArray())
+        val pAtt3 = MimeBodyPart(headers(cidAttach3, "text/plain; charset=utf-8", "attachment; filename=\"Att3.xml\""), "Русскiя 3333 аттача3".toByteArray())
+        mp.addBodyPart(pSoap)
+        mp.addBodyPart(pPayl)
+        mp.addBodyPart(pAtt1)
+        mp.addBodyPart(pAtt2)
+        mp.addBodyPart(pAtt3)
+
+        val ct2 = "multipart/related; boundary=\"$boundary\""
+        println()
+
+        val baos = ByteArrayOutputStream(16384)
+        mp.writeTo(baos)
+        baos.close()
+
+        println(messageid)
+        println(String(baos.toByteArray()))
+
+        if (true) {
+            println("************************************ Kotlin -> PO 7.5")
+            var con = URL(KT.testPO).openConnection() as HttpURLConnection
+            con.addRequestProperty("Content-Type", ct2)
+            con.addRequestProperty("Authorization", KT.authPO)
+            con.doOutput = true
+            con.connect()
+            var os = con.getOutputStream()
+            os.write(baos.toByteArray())
+            os.close()
+            var rc = con.responseCode
+            println(rc)
+
+            var iss = if (rc < 300) con.inputStream else con.errorStream
+            var ba = iss.readAllBytes()
+            println(String(ba))
+            iss.close()
+        }
+        if (false) {
+            println("************************************ Kotlin -> CPI Neo")
+            var con = URL(KT.testCPI).openConnection() as HttpsURLConnection
+            println(ct2)
+            con.addRequestProperty("Content-Type", ct2)
+            con.addRequestProperty("Authorization", KT.authCPI)
+            con.doOutput = true
+            con.connect()
+            var os = con.getOutputStream()
+            os.write(baos.toByteArray())
+            os.close()
+            var rc = con.responseCode
+            println(rc)
+
+            var iss = if (rc < 300) con.inputStream else con.errorStream
+            var ba = iss.readAllBytes()
+            println(String(ba))
+            iss.close()
+        }
+        if (false) {
+            println("************************************ Kotlin -> S4")
+            var con = URL(KT.testS4).openConnection() as HttpURLConnection
+            println(ct2)
+            con.addRequestProperty("Content-Type", ct2)
+            con.addRequestProperty("Authorization", KT.authS4)
+            con.doOutput = true
+            con.connect()
+            var os = con.getOutputStream()
+            os.write(baos.toByteArray())
+            os.close()
+            var rc = con.responseCode
+            println(rc)
+
+            var iss = if (rc < 300) con.inputStream else con.errorStream
+            var ba = iss.readAllBytes()
+            println(String(ba))
+            iss.close()
+        }
+    }
+    @Test
+    fun constructorS4() {
+        val messageid = "00000000-0000-0000-0000-000000000021"
+        val datetime = "2023-04-14T11:31:01Z"
+        val sender = XiMessage.PartyService(XiMessage.Party(), "TEST", XiMessage.Interface("", ""))
+        val iface = XiMessage.Interface("urn:nlmk:po:I:ERP-SD:Reports:Report", "SI_RequestReport_InAsync")
+        val receiver = XiMessage.PartyService(XiMessage.Party(), "DER200", iface)
+        val payload = """<ns0:MT_RequestReport xmlns:ns0="urn:nlmk:po:I:ERP-SD:Reports:Report">
+<SESSION_ID>$messageid</SESSION_ID>
+</ns0:MT_RequestReport>"""
+
+        val _msid = messageid.replace("-", "")
+        val cidSoap = "soap-$_msid@sap.com"
+        val cidPayl = "payload-$_msid@sap.com"
+
+        val main = XiMessage.Main(
+            3, 1, 1, null, "ApplicationMessage", "asynchronous", messageid, null, datetime,
+            sender, null, iface
+        )
+
+        val eo = XiMessage.ReliableMessaging(XiMessage.QOS.ExactlyOnce)
+
+        val payload1 = XiMessage.Payload("simple", "cid:$cidPayl", "MainDocument", XiMessage.PayloadType.Application)
+        val manifest = XiMessage.Manifest(null, listOf(payload1))
+        val envelope = XiMessage.Envelope(XiMessage.Header(main, eo), XiMessage.Body(manifest))
+        val smain = xmlserializer.encodeToString(envelope) + "\n"
+
+        val mp: MimeMultipart = MimeMultipart("related")
+        val ct = ContentType(mp.contentType)
+        val boundary = ct.getParameter("boundary")
+
+        val pSoap = MimeBodyPart(headers(cidSoap, "text/xml; charset=utf-8", "attachment; filename=\"$cidSoap.xml\""), smain.encodeToByteArray())
+        val pPayl = MimeBodyPart(headers(cidPayl, "application/xml; charset=utf-8", "attachment; filename=\"Payload.xml\""), payload.toByteArray())
+        mp.addBodyPart(pSoap)
+        mp.addBodyPart(pPayl)
+
+        val ct2 = "multipart/related; boundary=\"$boundary\""
+        println(messageid)
+        println(ct2)
+        mp.writeTo(System.out)
+    }
+
+    fun headers(contentId: String, contentType: String, contentDisposition: String): InternetHeaders {
+        val ih = InternetHeaders()
+        ih.addHeader("Content-ID", "<$contentId>")
+        ih.addHeader("Content-Type", contentType)
+        ih.addHeader("Content-Disposition", contentDisposition)
+        return ih
     }
 
     @Test
     fun parse() {
-        val xml = x("/pi_XI/message1.xml")
-        val z = KSoap.xmlserializer.parse<XiMessage.XSOAP>(xml)
+        println(xmlserializer.decodeFromReader<XiMessage.Envelope>(x("/pi_XI/message1.xml")))
+        println(xmlserializer.decodeFromReader<XiMessage.Envelope>(x("/pi_XI/message2.xml")))
+        println(xmlserializer.decodeFromReader<XiMessage.Envelope>(x("/pi_XI/message3.xml")))
     }
+
+    @Test
+    fun compose() {
+        val p1 = MimeBodyPart(ris("/pi_XI/message1.xml"))
+        val p2 = MimeBodyPart(ris("/pi_XI/message1.json"))
+        val mm = MimeMultipart(p1, p2)
+//        println(mm.contentType)
+//        mm.writeTo(System.out)
+        val ms = MimeMessage(Session.getDefaultInstance(Properties()))
+        val pa1 = MimeBodyPart()
+        val mpart: Multipart = MimeMultipart("related", pa1)
+        val soapPart = MimeBodyPart()
+        mpart.addBodyPart(soapPart)
+
+        println(mpart.toString())
+        mpart.writeTo(System.out)
+    }
+
+    @Test
+    fun rfc2387_1() {
+        val multipart: MimeMultipart = MimeMultipart()
+        multipart.setSubType("related")
+
+        val htmlPart = MimeBodyPart()
+        htmlPart.contentID = "<SOAP>"
+        htmlPart.setDescription("SOAP description")
+        // messageBody contains html that references image
+        // using something like <img src="cid:XXX"> where
+        // "XXX" is an identifier that you make up to refer
+        // to the image
+        // messageBody contains html that references image
+        // using something like <img src="cid:XXX"> where
+        // "XXX" is an identifier that you make up to refer
+        // to the image
+        htmlPart.setText("messageBody", "utf-8", "html")
+        multipart.addBodyPart(htmlPart)
+
+        val imgPart = MimeBodyPart()
+        imgPart.setDataHandler(DataHandler(ByteArrayDataSource("bytes", "text/xml")))
+        imgPart.attachFile("c:\\temp\\1.txt")
+        imgPart.contentID = "<PAYLOAD>"
+        multipart.addBodyPart(imgPart)
+
+        //message.setContent(multipart)
+        println(multipart.contentType)
+        multipart.writeTo(System.out)
+    }
+
+    @Test
+    fun rfc_2() {
+        val ih1 = InternetHeaders()
+        ih1.addHeader("Content-ID", "<soap-8670dd5ad76611ed94c2000000417ca6@sap.com>")
+        ih1.addHeader("Content-Disposition", "attachment;filename=\"soap-8670dd5ad76611ed94c2000000417ca6@sap.com.xml\"")
+        ih1.addHeader("Content-Type", "text/xml; charset=utf-8")
+        ih1.addHeader("Content-Description", "SOAP")
+        val ba1 = "<SOAP/>".encodeToByteArray()
+        val p1 = MimeBodyPart(ih1, ba1)
+
+        val ih2 = InternetHeaders()
+        ih2.addHeader("Content-Type", "multipart/related")
+        ih2.addHeader("Content-ID", "<payload-866f7675d76611ed89de000000417ca6@sap.com>")
+        ih2.addHeader("Content-Disposition", "attachment;filename=\"MainDocument.bin\"")
+        ih2.addHeader("Content-Description", "MainDocument")
+        val ba2 = "<MainDocument/>".encodeToByteArray()
+        val p2 = MimeBodyPart(ih2, ba2)
+
+        val mp: MimeMultipart = MimeMultipart("related")
+        mp.addBodyPart(p1)
+        mp.addBodyPart(p2)
+        mp.writeTo(System.out)
+    }
+
 }
